b8c2315b9f6587c0e69d73ce87f995dcc18891ea
evanchooly <jlee@antwerkz.com>
2015-02-05 23:59:44 -0500

    missing file .gitignore update

---
+++ tests/src/main/java/com/antwerkz/critter/test/AbstractPerson.java
@@ -0,0 +1,13 @@
+package com.antwerkz.critter.test;
+
+public class AbstractPerson {
+  private Long age;
+
+  public Long getAge() {
+    return age;
+  }
+
+  public void setAge(final Long age) {
+    this.age = age;
+  }
+}
