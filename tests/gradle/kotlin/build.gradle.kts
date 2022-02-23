f7e093b8134c928414fad27925553aa1e9cf0e64
evanchooly <jlee@antwerkz.com>
2022-02-22 21:29:55 -0500

    bump to release version 4.1.1

--- tests/gradle/kotlin/build.gradle.kts
+++ tests/gradle/kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     kotlin("jvm") version "1.6.10"
-    id("dev.morphia.critter") version "4.1.1-SNAPSHOT"
+    id("dev.morphia.critter") version "4.1.1"
 }
 
 repositories {
